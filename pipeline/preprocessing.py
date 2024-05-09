449d3edb2494aa05917d4d912844d3c5a02dd6b0
Hamilton-cic <cicerosantos082@gmail.com>
2024-05-09 01:22:28 +0100

    Update preprocessing.py

--- pipeline/preprocessing.py
+++ pipeline/preprocessing.py
@@ -1,4 +1,28 @@
-def refactoring(x = 'string') :
-    ''' retirar a primeira parte do nome das colunas que se repetem por varias colunas como "costumer_" ou "lifetime_"
-    '''
+def refactor_column_names(df):
+    """
+    Refactors the column names of a DataFrame by removing common prefixes.
+
+    This function checks if there are 3 or more columns starting with the same prefix 
+    (e.g., 'customer_' or 'lifetime_') and if so, removes that prefix from the column names.
+
+    Parameters:
+    df (pandas.DataFrame): The DataFrame whose column names are to be refactored.
+
+    Returns:
+    pandas.DataFrame: The DataFrame with refactored column names.
+    """
+    prefixes = {}
+    for col in df.columns:
+        prefix = col.split('_')[0] + '_'
+        if prefix not in prefixes:
+            prefixes[prefix] = [col]
+        else:
+            prefixes[prefix].append(col)
+
+    for prefix, cols in prefixes.items():
+        if len(cols) >= 3:
+            refactored_columns = {col: col.replace(prefix, '') for col in cols}
+            df.rename(columns=refactored_columns, inplace=True)
+
+    return df
     
